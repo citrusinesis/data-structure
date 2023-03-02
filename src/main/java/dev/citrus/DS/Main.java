e2a415691c6a52f5312c8f96eaf50f5eaa363fa3
citrusinesis <jihojiho2003@gmail.com>
2023-03-02 16:07:36 +0900

    make `RunnerInterface`

--- src/main/java/dev/citrus/DS/Main.java
+++ src/main/java/dev/citrus/DS/Main.java
@@ -1,7 +1,5 @@
 package dev.citrus.DS;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Data Structure");
---
+++ src/main/java/dev/citrus/DS/RunnerInterface.java
@@ -0,0 +1,5 @@
+package dev.citrus.DS;
+
+public interface RunnerInterface {
+    // TODO: 자료구조 구현체 인터페이스 구현, 테스트 코드로 동작할 수 있게 함
+}
