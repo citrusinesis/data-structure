f060d09a397c53f483bced25f4b4ccc55692a4e7
citrusinesis <jihojiho2003@gmail.com>
2023-03-31 04:03:20 +0900

    add jacoco: to check code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
 plugins {
     id("java")
+    jacoco
+}
+
+jacoco {
+    toolVersion = "0.8.8"
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+
+    dependsOn(tasks.test)
 }
 
 group = "dev.citrus.DS"
