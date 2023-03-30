9d5f9928329c1fdd506433e933371138b85474e3
citrusinesis <jihojiho2003@gmail.com>
2023-03-31 04:03:05 +0900

    chore: add entrypoint

--- src/main/java/dev/citrus/DS/Main.java
+++ src/main/java/dev/citrus/DS/Main.java
@@ -2,6 +2,6 @@ package dev.citrus.DS;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Data Structure");
+        System.out.println("Hello, Data Structure!");
     }
 }
