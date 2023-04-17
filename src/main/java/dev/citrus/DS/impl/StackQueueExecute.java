9ea117dce7329a92fb1b56a7559aa90409dc936d
citrusinesis <jihojiho2003@gmail.com>
2023-04-17 13:52:29 +0900

    add stack, queue implementation

---
+++ src/main/java/dev/citrus/DS/impl/StackQueueExecute.java
@@ -0,0 +1,17 @@
+package dev.citrus.DS.impl;
+
+import dev.citrus.DS.example.IntLinkedQueue;
+import dev.citrus.DS.example.IntLinkedStack;
+import dev.citrus.DS.example.IntQueue;
+import dev.citrus.DS.example.IntStack;
+
+public class StackQueueExecute {
+    public static void main(String[] args) {
+        IntStack stack = new IntStack();
+        IntLinkedStack linkedStack = new IntLinkedStack();
+        IntQueue queue = new IntQueue();
+        IntLinkedQueue linkedQueue = new IntLinkedQueue();
+
+
+    }
+}
